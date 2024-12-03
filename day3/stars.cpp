d843684a475cc179f95fcde1b1d0486b36cf0f7e
ash01825 <niteshtulshyan998@gmail.com>
2024-12-04 01:55:13 +0530

    day 3 star

--- day3/stars.cpp
+++ day3/stars.cpp
@@ -1,74 +1,34 @@
-//
-// Created by Arsh Tulshyan on 04/12/24.
-//
 #include <fstream>
-#include <iostream>
-#include <cctype>
-using namespace std;
+#include <stdio.h>
+#include <string>
 
-int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
 
-    ifstream input("/Users/ash/CLionProjects/AdventofCode/day3/input.txt");
-    if (!input.is_open()) {
-        cerr << "Error: Unable to open input file." << endl;
-        return 1;
+int main()    {
+    std::string text;
+    std::ifstream file("./input.txt");
+    std::string str = "";
+
+    while (getline(file, text)) {
+        str.append(text);
+        str.append("\n");// replace lost eol
     }
 
-    string word;
-    unsigned int ans = 0;
+    int iAns1 = 0;
+    int iAns2 = 0;
+    int i1, i2;
+    char c = 0;
+    bool on = true;
+    const char* p = str.data();
 
-    while (input >> word) {
-        for (size_t i = 0; i < word.length(); i++) {
-            if (word[i] == 'm')
-            {
-                if(word[i + 1] == 'u')
-                {
-                    if(word[i + 2] == 'l')
-                    {
-                        if(word[i + 3] == '('){
-                            string numChar1, numChar2;
-
-                            size_t j = i + 4;
-                            while (j < word.length() && word[j] != ',' && word[j] != ')') {
-                                if (!isdigit(word[j]))
-                                {
-                                    i=j-1;
-                                    continue;
-                                }
-                                numChar1 += word[j];
-                                j++;
-                            }
-
-                            j++;
-
-                            while (j < word.length() && word[j] != ')') {
-                                if (!isdigit(word[j]))
-                                {
-                                    i=j-1;
-                                    continue;
-                                }
-                                numChar2 += word[j];
-                                j++;
-                            }
-                            int num1 = stoi(numChar1);
-                            int num2 = stoi(numChar2);
-                            ans += num1 * num2;
-
-                            i = j;
-                        }
-                        else
-                            i+=2;
-                    }
-                    else
-                        i++;
-
-                }
-            }
+    while( *p ) {
+        if( sscanf( p, "mul(%d,%d%c", &i1, &i2, &c) == 3 && c == ')' ) {
+            iAns1 += i1 * i2;
+            if (on) iAns2 += i1 * i2;
         }
+        if( sscanf( p, "do(%c", &c) == 1 && c == ')' ) on = true;
+        if( sscanf( p, "don't(%c", &c) == 1 && c == ')' ) on = false;
+        p++;
     }
-    cout << "The result is: " << ans << endl;
-
-    return 0;
+    printf("Answer1 %d\n", iAns1);
+    printf("Answer2 %d\n", iAns2);
 }
