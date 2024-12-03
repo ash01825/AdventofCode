e7fca885793e56f7eb53100c22242964aed07125
ash01825 <niteshtulshyan998@gmail.com>
2024-12-03 11:08:01 +0530

    day 2 star

--- day2/stars.cpp
+++ day2/stars.cpp
@@ -2,6 +2,7 @@
 // Created by Arsh Tulshyan on 02/12/24.
 //
 
+
 #include <fstream>
 #include <iostream>
 #include <vector>
@@ -9,55 +10,98 @@
 
 using namespace std;
 
-int check_safety(const vector<int>& arr, int k) {
-    size_t n = arr.size();
 
-    bool increasing = arr[0] < arr[1];
-    bool decreasing = arr[0] > arr[1];
-    if (!increasing && !decreasing) return 0;
-
-    for (size_t i = 1; i < n - 1; ++i) {
-        int diff = arr[i + 1] - arr[i];
-        if (increasing && (diff <= 0 || diff >= 4))
-            return 0;
-        if (decreasing && (diff <= 0 || diff >= 4))
-            return 0;
+int check_safety(vector<int> &arr, bool k)
+{
+    const size_t n = arr.size();
+    if(arr[0] > arr[1] && arr[0] - arr[1]<4)
+    {
+        for(int i = 0; i < n-1; i++)
+            if((arr[i] <= arr[i+1])||arr[i]-arr[i+1]>3)
+            {
+                if(k)
+                {
+                    if(i==1)
+                {
+                    auto  b =  arr;
+                    b.erase(b.begin());
+                    if(check_safety(b,false)==1)
+                        return 1;
+                }
+                    auto  b =  arr;
+                    arr.erase(arr.begin()+i);
+                    if(check_safety(arr,false)==1)
+                        return 1;
+                    b.erase(b.begin()+i+1);
+                    return check_safety(b,false);
+                }
+                return 0;
+            }
+        return 1;
     }
-    return 1;
-}
+    if(arr[0] < arr[1] && arr[1] - arr[0]<4)
+    {
+        for(int i = 1; i < n-1; i++)
+            if((arr[i] >= arr[i+1])||arr[i+1]-arr[i]>3)
+            {
+                if(k)
+                {
+                    if(i==1)
+                    {
+                        auto  b =  arr;
+                        b.erase(b.begin());
+                        if(check_safety(b,false)==1)
+                            return 1;
+                    }
+                    auto  b =  arr;
+                    arr.erase(arr.begin()+i);
+                    if(check_safety(arr,false)==1)
+                        return 1;
+                    b.erase(b.begin()+i+1);
+                    return check_safety(b,false);
+                }
+                return 0;
+            }
 
+        return 1;
+    }
+    if(k)
+    {
+        auto  i =  arr;
+        arr.erase(arr.begin());
+        if(check_safety(arr,false)==1)
+            return 1;
+        i.erase(i.begin()+1);
+        return check_safety(i,false);
+    }
+
+
+    return 0;
+}
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    ifstream input("/Users/ash/CLionProjects/AdventofCode/day2/input.txt");
+    ifstream input("input.txt");
     if (!input.is_open()) {
         cerr << "Error: Unable to open input file." << endl;
         return 1;
     }
-
-    vector<vector<int>> matrix;
-    matrix.reserve(1000);
-
+    vector<vector<int> > matrix(1000);
     string line;
-    while (getline(input, line)) {
+    for (int i = 0; i < 1000; i++) {
+        getline(input, line);
         istringstream iss(line);
-        vector<int> row;
         int x;
-        while (iss >> x) {
-            row.push_back(x);
-        }
-        matrix.push_back(std::move(row));
+        while(iss>>x)
+            matrix[i].push_back(x);
     }
-
     input.close();
-
     int safe = 0;
-    for (const auto& row : matrix) {
-            safe += check_safety(row,0);
-
+    for (auto& row : matrix)
+    {
+        safe+=check_safety(row,true);
     }
-
     cout << safe << "\n";
     return 0;
 }
